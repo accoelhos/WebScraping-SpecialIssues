c002f9129c035544948cfd3ccba3923f7bda1973
Ana Carolina Sá <anacarolina.sacoelho@gmail.com>
2024-06-10 23:39:42 -0300

    feat: criação do webscraping

---
+++ WS_BigData.py
@@ -0,0 +1,36 @@
+#Rotina de busca de Special Issues para o Journal of Big Data
+import requests
+from bs4 import BeautifulSoup
+
+
+url = "https://journalofbigdata.springeropen.com"
+
+response = requests.get(url)
+
+if response.status_code == 200:
+
+    site = BeautifulSoup(response.text, 'html.parser')
+    special_issues_section = site.find('div', {'id': 'open+special+issues'})
+
+    if special_issues_section:
+        
+        paragraphs = special_issues_section.find_all('p')
+
+        
+        for paragraph in paragraphs:
+            
+            title_element = paragraph.find('a', class_='is-external')
+            if title_element:
+                
+                titulo = title_element.text.strip()
+                link = title_element['href']
+
+                
+                deadline_element = paragraph.find('em')
+                deadline = deadline_element.text.strip() if deadline_element else 'No deadline'
+
+                print(f"Title: {titulo}\nLink: {link}\n{deadline}\n\n")
+    else:
+        print("No special issues section found")
+else:
+    print("Failed to retrieve the page")
