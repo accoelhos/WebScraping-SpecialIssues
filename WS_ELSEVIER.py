11dbcddd4e58cc39b86d7e21d78cb1380d103a1b
Gabriel-gag <115362448+Gabriel-gag@users.noreply.github.com>
2024-09-02 20:39:39 -0300

    add webscraping for ELSEVIER

---
+++ WS_ELSEVIER.py
@@ -0,0 +1,76 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
+import pandas as pd
+
+# Configurações do WebDriver
+options = Options()
+#options.add_argument('--headless')
+options.add_argument('window-size=1200x600')
+
+# Inicie o navegador
+navegador = webdriver.Chrome(options=options)
+
+# Acesse a página desejada
+url = "https://www.sciencedirect.com/browse/calls-for-papers"
+navegador.get(url)
+
+# Espera até que o conteúdo seja carregado
+WebDriverWait(navegador, 15).until(
+    EC.presence_of_element_located((By.CSS_SELECTOR, "li.publication"))
+)
+
+# Extrair o conteúdo da página com Selenium
+page_content = navegador.page_source
+
+# Analisar o HTML com BeautifulSoup
+soup = BeautifulSoup(page_content, 'html.parser')
+
+# Lista para armazenar os dados
+data = []
+
+# Encontrar todas as seções de publicações
+publications = soup.find_all('li', class_='publication')
+
+for publication in publications:
+    # Extrair o título da publicação
+    title_element = publication.find('span', class_='anchor-text')
+    title = title_element.text.strip() if title_element else "Título não encontrado"
+    
+    # Extrair o nome da revista
+    journal_element = publication.find('p', class_='publication-text')
+    journal = journal_element.text.strip().split('•')[0].strip() if journal_element else "Revista não encontrada"
+    
+    # Extrair a data de submissão
+    deadline_element = publication.find_all('div', class_='text-s')
+    submission_deadline = "Data não encontrada"
+    
+    for element in deadline_element:
+        if 'Submission deadline:' in element.text:
+            strong_tag = element.find('strong')
+            if strong_tag:
+                submission_deadline = strong_tag.text.strip()
+                break
+    
+    # Extrair o link
+    link_element = publication.find('a', class_='anchor')
+    link = "Link não encontrado"
+    
+    if link_element and 'href' in link_element.attrs:
+        link = "https://www.sciencedirect.com" + link_element['href']
+    
+    # Adicionar os dados à lista
+    data.append([title, journal, submission_deadline, link])
+
+# Criar um DataFrame e salvar em Excel
+df = pd.DataFrame(data, columns=['Título', 'Revista', 'Submission Deadline', 'Link'])
+output_file_path = 'WS_ELSEVIER.xlsx'
+df.to_excel(output_file_path, index=False)
+
+print(f"Os resultados foram salvos na planilha: {output_file_path}")
+
+# Fechar o navegador
+navegador.quit()
